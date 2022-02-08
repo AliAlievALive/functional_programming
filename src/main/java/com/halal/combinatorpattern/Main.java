c38f9fb85ec1327169929aed092c0190e9ef4194
AliAlievALive <madarat@mail.ru>
2022-02-08 17:43:26 +0300

    combinator pattern

--- src/main/java/com/halal/combinatorpattern/Main.java
+++ src/main/java/com/halal/combinatorpattern/Main.java
@@ -2,6 +2,9 @@ package com.halal.combinatorpattern;
 
 import java.time.LocalDate;
 
+import static com.halal.combinatorpattern.CustomerRegistrationValidator.*;
+import static com.halal.combinatorpattern.CustomerRegistrationValidator.ValidationResult.*;
+
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer(
@@ -11,8 +14,20 @@ public class Main {
                 LocalDate.of(2000, 1, 1)
         );
 
-        System.out.println(new CustomerValidatorService().isValid(customer));
+//        System.out.println(new CustomerValidatorService().isValid(customer));
 
         //if valid we can store customer in DB
+
+        // Using combinator pattern
+        ValidationResult result = isEmailValid()
+                .and(isPhoneNumberValid())
+                .and(isAnAdult())
+                .apply(customer);
+
+        System.out.println(result);
+
+        if (result != SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
     }
 }
